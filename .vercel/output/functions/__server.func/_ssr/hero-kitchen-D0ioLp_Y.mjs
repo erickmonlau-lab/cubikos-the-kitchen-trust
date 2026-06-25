d2bab35bdb515bb2e94272e05c5c37998c5a942b
Erick <erickmonlau@gmail.com>
2026-06-25 17:01:46 +0200

    Fix Vercel build error by removing .vercel from git

--- .vercel/output/functions/__server.func/_ssr/hero-kitchen-D0ioLp_Y.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/hero-kitchen-D0ioLp_Y.js
-var hero_kitchen_default = "/assets/hero-kitchen-DIfcTxlP.webp";
-//#endregion
-export { hero_kitchen_default as t };
