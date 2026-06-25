161614bad83d634ef2f3014dfe5f0699d22dd094
Erick <erickmonlau@gmail.com>
2026-06-25 20:11:39 +0200

    Fix Vercel build block again

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CV9XtcPV.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CV9XtcPV.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/erick/.gemini/antigravity/scratch/cubikos-the-kitchen-trust/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-5FCBmqZ4.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-5FCBmqZ4.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/erick/.gemini/antigravity/scratch/cubikos-the-kitchen-trust/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-Br6Od8e4.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
