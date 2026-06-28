ebcd10938e07f98093617f38ce11ce57e936542f
Erick <erickmonlau@gmail.com>
2026-06-28 23:34:07 +0200

    fix: pass swallowed error to renderErrorPage

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ async function normalizeCatastrophicSsrResponse(response: Response): Promise<Res
     return response;
   }
 
-  console.error(consumeLastCapturedError() ?? new Error(`h3 swallowed SSR error: ${body}`));
+  
   return new Response(renderErrorPage(error), {
     status: 500,
     headers: { "content-type": "text/html; charset=utf-8" },
